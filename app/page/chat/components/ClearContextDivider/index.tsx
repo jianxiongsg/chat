c13a11e92d3e9668796ee6f69b1a799614d6dc82
wb-jx721745 <wb-jx721745@alibaba-inc.com>
2023-11-18 01:16:48 +0800

    feat: 组件拆分

---
+++ app/page/chat/components/ClearContextDivider/index.tsx
@@ -0,0 +1,23 @@
+import { useChatStore } from "@/app/store";
+import styles from "./index.module.scss";
+import Locale from "../../../../locales";
+
+export function ClearContextDivider() {
+  const chatStore = useChatStore();
+
+  return (
+    <div
+      className={styles["clear-context"]}
+      onClick={() =>
+        chatStore.updateCurrentSession(
+          (session) => (session.clearContextIndex = undefined),
+        )
+      }
+    >
+      <div className={styles["clear-context-tips"]}>{Locale.Context.Clear}</div>
+      <div className={styles["clear-context-revert-btn"]}>
+        {Locale.Context.Revert}
+      </div>
+    </div>
+  );
+}
